6d282e86cfaefed9fb52ac3fef3aa078e411c6d6
Elvina <elvinashm@gmail.com>
2021-04-01 21:21:53 +0600

    new components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TopicsComponent } from './topics/topics.component';
+import { TopicComponent } from './topic/topic.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { PostComponent } from './post/post.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfileComponent,
+    HomeComponent,
+    AboutComponent,
+    TopicsComponent,
+    TopicComponent,
+    CreatePostComponent,
+    PostComponent,
+    PostDetailComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
