e41c855db8a0fbfba362e74e4ace0dd5049cc726
Elvina <elvinashm@gmail.com>
2021-04-21 19:17:01 +0600

    small changes

--- front/src/app/posts.ts
+++ front/src/app/posts.ts
@@ -12,7 +12,7 @@ export const posts = [
     id: 2,
     header: 'New post 2',
     text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    date: '20.04.2021',
+    date: '21.04.2021',
     likes: 0,
     author: 'new admin',
     topic: 'Frontend'
